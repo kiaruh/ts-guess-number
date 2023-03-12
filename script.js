8bb9a3f1c1ccc5d85d28466a38bd9eadd1d4cce3
kiaruh <nikoz.li@gmail.com>
2023-03-12 13:53:34 -0300

    add some logics to the game

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 "use strict";
-var _a;
-console.log((_a = document.querySelector('.message')) === null || _a === void 0 ? void 0 : _a.textContent);
+// document.querySelector('.guess')!.value = '21';
+// document.querySelector('.highscore')!.textContent = 9999;
+function generateSecreteNumber() {
+    return Math.trunc(Math.random() * 20) + 1;
+}
+let secretNumber = generateSecreteNumber();
+let score = Number(document.querySelector('.score').textContent);
+document.querySelector('.number').textContent = String(secretNumber);
+const checkButton = document
+    .querySelector('.check')
+    .addEventListener('click', () => {
+    if (score > 1) {
+        const guess = +document.querySelector('.guess')
+            .value;
+        if (guess === secretNumber) {
+            document.querySelector('.message').textContent = '🎉 Correct Number!';
+            score = score + 10;
+        }
+        if (guess > secretNumber) {
+            document.querySelector('.message').textContent = '📈 Too High!';
+            score--;
+        }
+        if (guess < secretNumber) {
+            document.querySelector('.message').textContent = '📉 Too Low!';
+            score--;
+        }
+        if (!guess) {
+            document.querySelector('.message').textContent = '🚫 No Number';
+            score--;
+        }
+        document.querySelector('.score').textContent = String(score);
+    }
+    else {
+        score--;
+        document.querySelector('.score').textContent = String(score);
+        document.querySelector('h1').textContent = '😫 Game Over';
+    }
+});
+const againButton = document
+    .querySelector('.again')
+    .addEventListener('click', () => {
+    score = 20;
+    document.querySelector('.score').textContent = String(score);
+    document.querySelector('.message').textContent = 'Start guessing...';
+    secretNumber = generateSecreteNumber();
+    document.querySelector('.number').textContent = String(secretNumber);
+});
